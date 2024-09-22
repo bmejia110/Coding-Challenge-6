3d1b5c989e02e65acf89436f889fd8e80fb6a94d
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-22 16:42:28 -0400

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,8 @@
+//Task 1: Create an Employees Array of Employee Objects
+
+let employees = [
+    { name: "John", shifts:[{ day:"Monday" , hours: 5 }, { day: "Wednesday", hours: 6}] },
+    { name: "Nikki", shifts:[{ day:"Tuesday" , hours: 6 }, { day: "Thursday", hours: 7}] },
+    { name: "Tyler", shifts:[{ day:"Monday" , hours: 8 }, { day: "Saturday", hours: 4}] },
+    { name: "Myles", shifts:[{ day:"Wednesday" , hours: 6 }, { day: "Sunday", hours: 5}] },
+];
